de5701795421df3417d2af076a96f0e9591a34c7
Sanjay Kumar <74352452+1242sanjay@users.noreply.github.com>
2022-08-16 22:22:03 +0530

    Add files via upload

---
+++ static/assets/js/blog.js
@@ -0,0 +1,11 @@
+function signup() {
+	alert("You have signed up successfully");
+}
+
+function login() {
+	alert("You have logged in successfully");
+}
+
+function login() {
+	alert("Subscribed successfully!"+<br/>+"Now we'll send you notifications about our new posts.");
+}
